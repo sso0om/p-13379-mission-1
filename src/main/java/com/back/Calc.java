ca5a0afd8ca3124f75eec9b2011b8fcd36084afd
sso0om <ssooom142@gmail.com>
2025-05-31 20:39:32 +0900

    feat: 빼기 기능 추가

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,10 +2,13 @@ package com.back;
 
 public class Calc {
     public static int run(String exp) {
-        String[] inputs = exp.split("\\s\\+\\s");
+        String[] inputs = exp.split("\\s[+-]\\s");
         int number1 = Integer.parseInt(inputs[0]);
         int number2 = Integer.parseInt(inputs[1]);
 
-        return number1 + number2;
+        if (exp.contains("+")) return number1 + number2;
+        if (exp.contains("-")) return number1 - number2;
+
+        return -1;
     }
 }
