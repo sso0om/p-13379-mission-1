0d044e67cfa452d2dbc3a7b849de3e66a51ae3d4
sso0om <ssooom142@gmail.com>
2025-05-31 20:35:26 +0900

    feat: 더하기 기능 추가

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,4 +1,11 @@
 package com.back;
 
 public class Calc {
+    public static int run(String exp) {
+        String[] inputs = exp.split("\\s\\+\\s");
+        int number1 = Integer.parseInt(inputs[0]);
+        int number2 = Integer.parseInt(inputs[1]);
+
+        return number1 + number2;
+    }
 }
--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -1,7 +1,33 @@
 package com.back;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CalcTest {
 
+    @Test
+    @DisplayName("1 + 1 == 2")
+    void t1() {
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("2 + 1 == 3")
+    void t2() {
+        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("2 + 2 == 4")
+    void t3() {
+        assertThat(Calc.run("2 + 2")).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("1000 + 280 == 1280")
+    void t4() {
+        assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
+    }
 }
